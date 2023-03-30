07ab3f9b40ad7f118301eaf193415f08c54c0600
nisarnis <nisarnijar97@gmail.com>
2023-03-30 13:28:05 +0530

    nandy added

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -5,4 +5,15 @@ private void employee() {
 String name="nisar";
 System.out.println(name);
 }
+private void employeeAddress(String Add) {
+System.out.println(Add);
+}
+
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.employeeAddress("Trichy");
+}
+
+
+
 }
