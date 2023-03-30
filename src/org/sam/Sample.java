a78b6aed95e0314505dd56816ce243ca9f08e4bf
nisarnis <nisarnijar97@gmail.com>
2023-03-30 13:05:27 +0530

    priyanka added

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -1,8 +1,15 @@
 package org.sam;
 
 public class Sample {
-private void employee() {
-String name="nisar";
-System.out.println(name);
-}
+	private void employee() {
+		String name = "nisar";
+		System.out.println(name);
+	}
+
+	private void employeeName(String name1, String name2) {
+		name1 = "nandy";
+		name2 = "priyanka";
+		System.out.println(name1 + "/n" + name2);
+
+	}
 }
